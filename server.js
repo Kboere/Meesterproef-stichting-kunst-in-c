8de56609d3f86e2fa295ecad0c984b06579c121e
Kboere <kevin.boere@hva.nl>
2024-05-17 11:56:25 +0200

    start of project

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+
+// Set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+// Define routes
+app.get("/", async function (req, res) {
+  try {
+    
+    res.render("pages/index");
+  } catch (err) {
+    console.error("Error retrieving page:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
