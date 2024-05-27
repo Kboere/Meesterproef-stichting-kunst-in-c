733a65550533e8f0d96350d457ec16be22786fa2
Kboere <kevin.boere@hva.nl>
2024-05-27 17:32:14 +0200

    added navbar on scroll

---
+++ public/js/kevin.js
@@ -0,0 +1,12 @@
+const scrollHeader = document.querySelector('header');
+const logo = document.querySelector('.logo svg');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) { // Adjust this value based on when you want the color to change
+        logo.classList.add('active');
+        scrollHeader.classList.add('active');
+    } else {
+        logo.classList.remove('active');
+        scrollHeader.classList.remove('active');
+    }
+});
