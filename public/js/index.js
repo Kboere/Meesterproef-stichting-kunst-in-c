6e918fd7f7ea0e15bf22a16ee4f2ebc393a3f3c7
Kboere <kevin.boere@hva.nl>
2024-05-28 14:35:45 +0200

    small fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,21 @@
+const scrollHeader = document.querySelector('header');
+const nav = document.querySelector('nav');
   const menu = document.getElementById("mobileMenu");
+  const menuUl = document.querySelector("#mobileMenu ul");
   const hamburger = document.querySelector(".hamburger");
   const logo = document.querySelector(".logo svg");
 
-//   const hamburgerLi = document.querySelectorAll("#mobileMenu ul li");
-//   const hamburgerLiA = document.querySelectorAll("#mobileMenu ul li a");
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) { // Adjust this value based on when you want the color to change
+        logo.classList.add('active');
+        scrollHeader.classList.add('active');
+        menuUl.classList.add('active');
+    } else {
+        logo.classList.remove('active');
+        scrollHeader.classList.remove('active');
+        menuUl.classList.remove('active');
+    }
+});
 
   function toggleMenu() {
     menu.classList.toggle("show");
@@ -12,12 +24,10 @@
 
   hamburger.addEventListener("click", () => {
     if (hamburger.classList.contains("active")) {
-      logo.classList.remove("active");
       menu.style.display = "none";
 
     } else {
-      logo.classList.add("active");
-      menu.style.display = "block";
+      menu.style.display = "flex";
     }
 
     toggleMenu();
