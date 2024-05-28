e15a38b5ce2d5745211c24cfee12cda7a5bb7a37
Kboere <kevin.boere@hva.nl>
2024-05-28 15:02:24 +0200

    added JS function

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,34 @@
-function toggleMenu() {
-    const menu = document.getElementById('mobileMenu');
-    const hamburger = document.querySelector('.hamburger');
-    menu.classList.toggle('show');
-    hamburger.classList.toggle('active');
+const scrollHeader = document.querySelector('header');
+const nav = document.querySelector('nav');
+  const menu = document.getElementById("mobileMenu");
+  const menuUl = document.querySelector("#mobileMenu ul");
+  const hamburger = document.querySelector(".hamburger");
+  const logo = document.querySelector(".logo svg");
 
-    
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) { // Adjust this value based on when you want the color to change
+        logo.classList.add('active');
+        scrollHeader.classList.add('active');
+        menuUl.classList.add('active');
+    } else {
+        logo.classList.remove('active');
+        scrollHeader.classList.remove('active');
+        menuUl.classList.remove('active');
+    }
+});
 
+  function toggleMenu() {
+    menu.classList.toggle("show");
+    hamburger.classList.toggle("active");
   }
+
+  hamburger.addEventListener("click", () => {
+    if (hamburger.classList.contains("active")) {
+      menu.style.display = "none";
+
+    } else {
+      menu.style.display = "flex";
+    }
+
+    toggleMenu();
+  });
