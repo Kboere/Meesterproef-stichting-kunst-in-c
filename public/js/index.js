26ccfda6c2fec89869bfce3ce788a43ff88c184d
Kboere <kevin.boere@hva.nl>
2024-05-29 11:42:41 +0200

    anim on darkmode lamp

--- public/js/index.js
+++ public/js/index.js
@@ -4,16 +4,22 @@ const nav = document.querySelector('nav');
   const menuUl = document.querySelector("#mobileMenu ul");
   const hamburger = document.querySelector(".hamburger");
   const logo = document.querySelector(".logo svg");
+  const darkmodeLamp = document.getElementById("lamp-svgrepo-com");
+  const darkmodeLampLine = document.querySelector("#Group_6 #Path_79");
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 1) { // Adjust this value based on when you want the color to change
         logo.classList.add('active');
         scrollHeader.classList.add('active');
         menuUl.classList.add('active');
+        darkmodeLamp.classList.add('active');
+        darkmodeLampLine.classList.add('active');
     } else {
         logo.classList.remove('active');
         scrollHeader.classList.remove('active');
         menuUl.classList.remove('active');
+        darkmodeLamp.classList.remove('active');
+        darkmodeLampLine.classList.remove('active');
     }
 });
 
