e1948b2cf9478cd087ff5619fe28b3c50eca5b4d
Kboere <kevin.boere@hva.nl>
2024-05-29 12:20:29 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,40 @@
-const scrollHeader = document.querySelector('header');
-const nav = document.querySelector('nav');
-  const menu = document.getElementById("mobileMenu");
-  const menuUl = document.querySelector("#mobileMenu ul");
-  const hamburger = document.querySelector(".hamburger");
-  const logo = document.querySelector(".logo svg");
-  const darkmodeLamp = document.getElementById("lamp-svgrepo-com");
-  const darkmodeLampLine = document.querySelector("#Group_6 #Path_79");
+const scrollHeader = document.querySelector("header");
+const nav = document.querySelector("nav");
+const menu = document.getElementById("mobileMenu");
+const menuUl = document.querySelector("#mobileMenu ul");
+const hamburger = document.querySelector(".hamburger");
+const logo = document.querySelector(".logo svg");
+const darkmodeLamp = document.getElementById("lamp-svgrepo-com");
+const darkmodeLampLine = document.querySelector("#Group_6 #Path_79");
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 1) { // Adjust this value based on when you want the color to change
-        logo.classList.add('active');
-        scrollHeader.classList.add('active');
-        menuUl.classList.add('active');
-        darkmodeLamp.classList.add('active');
-        darkmodeLampLine.classList.add('active');
-    } else {
-        logo.classList.remove('active');
-        scrollHeader.classList.remove('active');
-        menuUl.classList.remove('active');
-        darkmodeLamp.classList.remove('active');
-        darkmodeLampLine.classList.remove('active');
-    }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    // Adjust this value based on when you want the color to change
+    logo.classList.add("active");
+    scrollHeader.classList.add("active");
+    menuUl.classList.add("active");
+    darkmodeLamp.classList.add("active");
+    darkmodeLampLine.classList.add("active");
+  } else {
+    logo.classList.remove("active");
+    scrollHeader.classList.remove("active");
+    menuUl.classList.remove("active");
+    darkmodeLamp.classList.remove("active");
+    darkmodeLampLine.classList.remove("active");
+  }
 });
 
-  function toggleMenu() {
-    menu.classList.toggle("show");
-    hamburger.classList.toggle("active");
+function toggleMenu() {
+  menu.classList.toggle("show");
+  hamburger.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", () => {
+  if (hamburger.classList.contains("active")) {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
   }
 
-  hamburger.addEventListener("click", () => {
-    if (hamburger.classList.contains("active")) {
-      menu.style.display = "none";
-
-    } else {
-      menu.style.display = "flex";
-    }
-
-    toggleMenu();
-  });
+  toggleMenu();
+});
